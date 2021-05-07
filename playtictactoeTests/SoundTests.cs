0038ed2f7feeba20cbad58ef83fd382ab39b98db
Olya Prkhorova <prohoroa0695@gmail.com>
2021-05-07 13:59:17 +0300

    Add project files.

---
+++ playtictactoeTests/SoundTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
+namespace playtictactoeTests
+{
+    class SoundTests
+    {
+        private IWebDriver _webDriver;
+        private GamePage _gamePage;
+        private VisibilityElement _visibilityElement;
+
+        [SetUp]
+        public void SetUp()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            _webDriver = new ChromeDriver();
+
+            _visibilityElement = new VisibilityElement();
+
+            _gamePage = new GamePage(_webDriver);
+            _gamePage.GoToGamePage();
+        }
+
+        [Test]
+        public void Test()
+        {
+            //_gamePage.MuteSound();
+
+            Assert.That(_visibilityElement.existsElement());
+        }
+    }
+}
---
+++ playtictactoeTests/VisibilityElement.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace playtictactoeTests
+{
+    class VisibilityElement
+    {
+        private readonly IWebDriver _webDriver;
+        public VisibilityElement()
+        {
+
+        }
+
+        public bool existsElement()
+        {
+            try
+            {
+                _webDriver.FindElement(By.CssSelector("[d=M67.41, 34.095l - 4.243, 4.239c6.43, 6.432, 6.43, 16.896, 0, 23.324L67.41, 65.9C76.179, 57.129, 76.179, 42.864, 67.41, 34.095z]"));
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
