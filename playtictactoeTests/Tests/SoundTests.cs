45aaa19fbb2360b8b63d15f96e37afb48bd717f6
Olya Prkhorova <prohoroa0695@gmail.com>
2021-05-10 23:52:04 +0300

    /

--- playtictactoeTests/Tests/SoundTests.cs
+++ playtictactoeTests/Tests/SoundTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -10,7 +11,7 @@ using WebDriverManager.Helpers;
 
 namespace playtictactoeTests
 {
-    class SoundTests
+    public class SoundTests
     {
         private IWebDriver _webDriver;
         private GamePage _gamePage;
@@ -21,19 +22,40 @@ namespace playtictactoeTests
         {
             new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
             _webDriver = new ChromeDriver();
+            _webDriver.Manage().Window.Maximize();
+            _constMethods = new ConstMethods(_webDriver);
 
             _gamePage = new GamePage(_webDriver);
         }
 
         [Test]
-        public void Test()
+        public void MuteSound()
         {
             var expectedResult = true;
 
             _gamePage.GoToGamePage()
                 .MuteSound();
 
-            Assert.AreEqual(expectedResult, _constMethods.VisibilityOfElement(By.CssSelector("[d ^= 'M67']")));
+            Assert.AreEqual(expectedResult, _constMethods.InvisibilityOfElement(By.CssSelector("[d^= 'M67']")));
+        }
+
+        [Test]
+        public void UnMuteSound()
+        {
+            var expectedResult = true;
+
+            _gamePage.GoToGamePage()
+                .MuteSound();
+            Thread.Sleep(3000);
+            _gamePage.MuteSound();
+
+            Assert.AreEqual(expectedResult, _constMethods.VisibilityOfElement(By.CssSelector("[d^=M67]")));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Quit();
         }
     }
 }
